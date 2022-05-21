6fe63e92b0f9c8486feeea3429266408055174b0
lporto2 <you@example.com>
2022-05-20 20:14:51 -0400

    Exercicios aula 34

---
+++ Exercicios Aula 34/teste 3.js
@@ -0,0 +1,27 @@
+let opcao = 2;
+
+switch (opcao) {
+    case 0:
+    console.log("Para funções de abertura de conta");
+    break;
+
+    case 1:
+        console.log("Para funções da minha conta");
+        break;
+        
+        case 2: 
+        console.log("Para funções de cartão");
+        break;
+
+        case 3:
+            console.log("Para funções de faturas");
+            break;
+
+            case 4:
+                console.log("Para funções de reclamações");
+                break;
+
+                default:
+                    console.log("Opção Inválida");
+                    break;
+}
---
+++ Exercicios Aula 34/teste 4.js
@@ -0,0 +1,4 @@
+var opcao = 2 
+
+typeof(opcao) 
+console.log(typeof(opcao))

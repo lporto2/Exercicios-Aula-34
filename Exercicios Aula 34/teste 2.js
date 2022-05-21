2039a6b137d38a19e6041103cd7a7c46e9913059
lporto2 <you@example.com>
2022-05-20 20:04:41 -0400

    Exercicios Aula 34

---
+++ Exercicios Aula 34/teste 2.js
@@ -0,0 +1,23 @@
+let opcao;
+
+opcao = "L";
+
+switch (opcao) {
+    case "H":
+        console.log("Hamburguer");
+        break;
+
+        case "C":
+            console.log("Coxinha");
+            break;
+
+            case "P":
+                console.log("Pizza");
+                break;
+                default:
+                    console.log("Opção Não Existente no Cardápio");
+                    break;
+
+                    
+}
+

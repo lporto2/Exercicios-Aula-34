4a519abc541fb0e95780d0a754d01b54918db494
lporto2 <you@example.com>
2022-05-20 22:55:51 -0400

    Exercicios Aula 35

---
+++ Exercicios Aula 35/teste 6.js
@@ -0,0 +1,33 @@
+let carros = [
+    {
+
+        modelo: "celta",
+        cor: "preto",
+        ano: "2014"
+    },
+
+    {
+        modelo: "uno super-sônico",
+        cor: "verde",
+        ano: 2022
+    },
+    
+    {
+        modelo: "marea bomba",
+        cor: "amarelo",
+        ano: 2001
+    },
+    {
+        modelo: "lancer do Braia",
+        cor: "rosa",
+        ano: 1993
+    }
+
+]
+
+
+for (let carro of carros){
+    for(propriedade in carro){
+        console.log(carro[propriedade]);
+    }
+}
